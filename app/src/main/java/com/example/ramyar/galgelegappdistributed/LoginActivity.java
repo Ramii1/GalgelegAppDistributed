41336ca42797bfd9c99fe97a73fad0026273ea22
DESKTOP-ANDDVIF\Ramyar <ramyar6@hotmail.com>
2017-05-03 14:59:56 +0200

    login xml

---
+++ app/src/main/java/com/example/ramyar/galgelegappdistributed/LoginActivity.java
@@ -0,0 +1,10 @@
+package com.example.ramyar.galgelegappdistributed;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Ramyar on 02-05-2017.
+ */
+
+public class Login extends AppCompatActivity {
+}
